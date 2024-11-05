3333e4d7322d514df1c9f0145090cb1a09c5588d
Tsukishima1 <Tsukishima1@users.noreply.github.com>
2024-11-05 01:09:39 +0000

    deploy: cdbd726c33c9405b1287d44ab43ff76be9f60723

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button'; // src/Button/index.tsx
-export { default as Input } from './Input'; // src/Input/index.tsx
-export { Select, SelectContent, SelectItem, SelectTrigger } from './Select'; // src/Select/index.tsx
-export { default as Tooltip } from './Tooltip'; // src/Tooltip/index.tsx
