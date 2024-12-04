d85159e187f6e148b3f313dadbc19744a7b03369
Tsukishima1 <1732124764@qq.com>
2024-12-04 13:20:57 +0800

    docs(www): 恢复代码块默认字体

---
+++ docs-dist/meta__Input.ed7b4571.async.js
@@ -0,0 +1,16 @@
+"use strict";(self.webpackChunkhara_ui=self.webpackChunkhara_ui||[]).push([[1808],{56482:function(u,e,a){var t;a.r(e),a.d(e,{demos:function(){return c}});var d=a(17061),o=a.n(d),I=a(17156),p=a.n(I),r=a(67294),h=a(32465),s=a(11045),c={"input-demo-demo":{component:r.memo(r.lazy(function(){return a.e(2091).then(a.bind(a,90118))})),asset:{type:"BLOCK",id:"input-demo-demo",refAtomIds:["Input"],dependencies:{"index.tsx":{type:"FILE",value:a(77045).Z},"hara-ui":{type:"NPM",value:"0.1.0"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"Input \u793A\u4F8B"},context:{"hara-ui":s,react:t||(t=a.t(r,2))},renderOpts:{compile:function(){var _=p()(o()().mark(function i(){var l,v=arguments;return o()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,a.e(4019).then(a.bind(a,4019));case 2:return n.abrupt("return",(l=n.sent).default.apply(l,v));case 3:case"end":return n.stop()}},i)}));function m(){return _.apply(this,arguments)}return m}()}}}},54047:function(u,e,a){a.r(e),a.d(e,{texts:function(){return d}});var t=a(32465);const d=[{value:"\u8BE5\u7EC4\u4EF6\u5C01\u88C5\u4E86\u539F\u751F\u7684",paraId:0,tocIndex:0},{value:"input",paraId:0,tocIndex:0},{value:"\u6807\u7B7E\uFF0C\u63D0\u4F9B\u4E86\u66F4\u591A\u7684\u6837\u5F0F\u548C\u529F\u80FD\u3002",paraId:0,tocIndex:0},{value:"\u8F93\u5165\u6846\u7EC4\u4EF6\u7528\u4E8E\u63A5\u6536\u7528\u6237\u8F93\u5165\u7684\u6587\u672C\u4FE1\u606F\u3002",paraId:1,tocIndex:0},{value:`import { Input } from 'hara-ui';
+`,paraId:2,tocIndex:4},{value:`<Input />
+`,paraId:3,tocIndex:5},{value:"\u5C5E\u6027",paraId:4,tocIndex:6},{value:"\u7C7B\u578B",paraId:4,tocIndex:6},{value:"\u5FC5\u9009",paraId:4,tocIndex:6},{value:"\u9ED8\u8BA4\u503C",paraId:4,tocIndex:6},{value:"\u63CF\u8FF0",paraId:4,tocIndex:6},{value:"style",paraId:4,tocIndex:6},{value:"React.CSSProperties",paraId:4,tocIndex:6},{value:"\u5426",paraId:4,tocIndex:6},{value:"-",paraId:4,tocIndex:6},{value:"\u81EA\u5B9A\u4E49\u6837\u5F0F",paraId:4,tocIndex:6},{value:"className",paraId:4,tocIndex:6},{value:"string",paraId:4,tocIndex:6},{value:"\u5426",paraId:4,tocIndex:6},{value:"-",paraId:4,tocIndex:6},{value:"\u81EA\u5B9A\u4E49\u7C7B\u540D",paraId:4,tocIndex:6}]},77045:function(u,e){e.Z=`import { Input } from 'hara-ui';
+import React from 'react';
+
+export default () => {
+  return (
+    <>
+      <Input placeholder="\u8BF7\u8F93\u5165\u7528\u6237\u540D" />
+      <Input type="password" placeholder="\u8BF7\u8F93\u5165\u5BC6\u7801" />
+      <Input type="file" />
+      <Input disabled placeholder="\u88AB\u7981\u7528\u7684\u8F93\u5165\u6846" />
+    </>
+  );
+};
+`}}]);
